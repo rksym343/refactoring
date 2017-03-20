d83906e8a50096b3fe8fd9b84dd87ff04a2d363f
lkm <rksym343@naver.com>
2017-03-20 10:22:55 +0900

    movie class 정의 완료

---
+++ src/refactoring/dto/Movie.java
@@ -0,0 +1,25 @@
+package refactoring.dto;
+
+public class Movie {
+	private String title;
+	private int priceCode;
+
+	public Movie(String title, int priceCode) {
+		super();
+		this.title = title;
+		this.priceCode = priceCode;
+	}
+
+	public int getPriceCode() {
+		return priceCode;
+	}
+
+	public void setPriceCode(int priceCode) {
+		this.priceCode = priceCode;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+}
